13f80d4caa2bcaedd1ceb6ec6de21be609392bfc
simo-andreev <simeon.zlatanov.andreev@gmail.com>
2018-09-02 01:48:23 +0300

    Add packages for web an annotation/annotProcessing

--- src/main/kotlin/bg/o/sim/web/TransactionRepo.kt
+++ src/main/kotlin/bg/o/sim/web/TransactionRepo.kt
@@ -1,5 +1,7 @@
-package bg.o.sim
+package bg.o.sim.web
 
+import bg.o.sim.BaseEntity
+import bg.o.sim.CrudApiController
 import bg.o.sim.web.BaseEntity
 import bg.o.sim.web.CrudApiController
 import org.springframework.beans.factory.annotation.Autowired
